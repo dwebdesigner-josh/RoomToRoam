b2f701fe06226498ca73b2063e5bf12503b4bed6
Josh Windows <greateagle87@gmail.com>
2024-05-15 23:20:41 -0400

    feat: swirl1 - Adobe AE JSON animation created and optimized to 1.45mb | feat: test run of swirl 1 in blank main doc

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('swirl1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../scripts/swirl1/swirl1.json'
+  })
+
+  
