e6727a43cd4e66b695d1fecd21143bb6bca83c7b
Josh Windows <greateagle87@gmail.com>
2024-06-04 05:31:13 -0400

    fix: hamburger.js function fixed to set aria-expanded to the correct button upon clicking the close button

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,11 +1,18 @@
 (() => {
 
 const buttonopen = document.querySelector('#topnav-open');
+const buttonclose = document.querySelector('#topnav-close');
 
 function openMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "true");
 }
 
+function closeMobileMenu(){
+    buttonopen.setAttribute("aria-expanded", "false");
+}
+
 buttonopen.addEventListener('click', openMobileMenu);
+buttonclose.addEventListener('click', closeMobileMenu);
+
 
 })();
