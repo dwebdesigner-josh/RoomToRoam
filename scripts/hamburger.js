1baea0cfbaff849efedf45e9bcec7be5df9e85d2
Josh Windows <greateagle87@gmail.com>
2024-06-04 03:13:26 -0400

    fix: nav height vw value removed (this was expanding it too far and blocking other buttons on the page below)

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,11 @@
-const buttonopen = document.querySelector('#topnav-open');
+(() => {
+
+const buttonopen = document.querySelector('button#topnav-open');
 
 function openMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "true");
 }
 
 buttonopen.addEventListener('click', openMobileMenu);
+
+})();
