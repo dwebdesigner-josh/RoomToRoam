36afcf3f4b1235e47b29a28f112c1b61d40d92c0
Josh Windows <greateagle87@gmail.com>
2024-06-04 06:06:09 -0400

    feat: added further accessibility features to nav in main.css and hamburger.js

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,17 +2,60 @@
 
 const buttonopen = document.querySelector('#topnav-open');
 const buttonclose = document.querySelector('#topnav-close');
+const media = window.matchMedia('(width < 850px)');
+const topNavMenu = document.querySelector('#topnav-menu');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
 
 function openMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "true");
+    topNavMenu.removeAttribute('inert');
+    topNavMenu.removeAttribute('style');
+    main.setAttribute('inert', '');
+    buttonclose.focus();
 }
 
 function closeMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "false");
+    topNavMenu.setAttribute('inert', '');
+    main.removeAttribute('inert');
+    buttonopen.focus();
+    
+  setTimeout(() => {
+    topNavMenu.style.transition = 'none';
+  }, 500);
 }
+
+
+
+function setupTopNav(e) {
+    if (e.matches) {
+      // is mobile
+      console.log('is mobile');
+      topNavMenu.setAttribute('inert', '');
+      topNavMenu.style.transition = 'none';
+    } else {
+      // is tablet/desktop
+      console.log('is desktop');
+      closeMobileMenu();
+      topNavMenu.removeAttribute('inert');
+    }
+  }
+  
+  setupTopNav(media);
+
 
 buttonopen.addEventListener('click', openMobileMenu);
 buttonclose.addEventListener('click', closeMobileMenu);
 
 
+
+media.addEventListener('change', function (e) {
+    setupTopNav(e);
+  });
+
 })();
+
+
+
+
