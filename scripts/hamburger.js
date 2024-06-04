07d888fc4442fe6dca3336f0018de8dd55a56279
Josh Windows <greateagle87@gmail.com>
2024-06-04 03:06:30 -0400

    feat: continued work on nav

---
+++ scripts/hamburger.js
@@ -0,0 +1,7 @@
+const buttonopen = document.querySelector('#topnav-open');
+
+function openMobileMenu(){
+    buttonopen.setAttribute("aria-expanded", "true");
+}
+
+buttonopen.addEventListener('click', openMobileMenu);
