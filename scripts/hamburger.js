ac4b6f73372560c0fa1f2235fcea727b36a6f3e9
Josh Windows <greateagle87@gmail.com>
2024-06-05 02:15:02 -0400

    fix: removed inert on main with nav opening and closing and replaced with a forEach loop that sets each individual review link tabindex to -1 to avoid being in the tab index but still clickable while the menu is open for mouse users

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -6,28 +6,50 @@ const media = window.matchMedia('(width < 850px)');
 const topNavMenu = document.querySelector('#topnav-menu');
 const main = document.querySelector('main');
 const body = document.querySelector('body');
+const review = document.querySelectorAll('.review-link'); //fixed: The querySelector() method only returns the first element that matches the specified selectors (first element in the class). To return all the matches, use the querySelectorAll() method instead.
 
 function openMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "true");
     topNavMenu.removeAttribute('inert');
     topNavMenu.removeAttribute('style');
     buttonopen.setAttribute('inert','');
-    main.setAttribute('inert', '');
-    buttonclose.focus();
+   // main.setAttribute('inert', '');       - TODO consider reimplementing or keeping removed
+   buttonclose.focus();
+  // review.setAttribute('tabindex','-1');
+   
+   review.forEach(nodelistitem => {
+    nodelistitem.setAttribute('tabindex', '-1');
+   });
+
 }
 
 function closeMobileMenu(){
     buttonopen.setAttribute("aria-expanded", "false");
     topNavMenu.setAttribute('inert', '');
-    main.removeAttribute('inert');
+  //  main.removeAttribute('inert');        - TODO consider reimplementing or keeping removed
     buttonopen.removeAttribute('inert');
     buttonopen.focus();
+    //review.removeAttribute('tabindex');
     
+ 
+    review.forEach(nodelistitem => {
+      nodelistitem.removeAttribute('tabindex');
+    });
+ 
+
   setTimeout(() => {
     topNavMenu.style.transition = 'none';
   }, 500);
 }
 
+function closeonclick(){
+  if (buttonopen.getAttribute('aria-expanded') === 'true'){
+    closeMobileMenu();
+  }
+  else {}
+};
+
+//if( $('.classname').getAttribute('aria-expanded') === 'true') {} https://stackoverflow.com/questions/37125674/how-to-check-for-attribute-value
 
 
 function setupTopNav(e) {
@@ -47,14 +69,17 @@ function setupTopNav(e) {
   setupTopNav(media);
 
 
+
 buttonopen.addEventListener('click', openMobileMenu);
 buttonclose.addEventListener('click', closeMobileMenu);
 
+main.addEventListener('click', closeonclick);
 
 
 media.addEventListener('change', function (e) {
     setupTopNav(e);
   });
+ 
 
 })();
 
