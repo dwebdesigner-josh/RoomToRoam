cec94e811eb2decf067fec6c4f6e9632dabe9367
Josh Windows <greateagle87@gmail.com>
2024-06-10 05:19:47 -0400

    test2fu

--- scripts/ham2.js
+++ scripts/ham2.js
@@ -3,30 +3,25 @@
     const buttonClose = document.querySelector('#topnav-close');
     const topNavMenu = document.querySelector('#topnav-menu');
   
-    function openMobileMenu() {
-      console.log('Open menu triggered');
-      topNavMenu.classList.add('open');
-      buttonOpen.setAttribute('aria-expanded', 'true');
-      buttonClose.focus();
-    }
-  
-    function closeMobileMenu() {
-      topNavMenu.classList.remove('open');
-      buttonOpen.setAttribute('aria-expanded', 'false');
-      buttonOpen.focus();
-    }
-  
-    function closeOnClick() {
-      if (topNavMenu.classList.contains('open')) {
-        closeMobileMenu();
+    function toggleMobileMenu() {
+      const isOpen = topNavMenu.classList.contains('open');
+      topNavMenu.classList.toggle('open', !isOpen);
+      buttonOpen.setAttribute('aria-expanded', !isOpen);
+      if (!isOpen) {
+        buttonClose.focus();
+      } else {
+        buttonOpen.focus();
       }
     }
   
-    buttonOpen.addEventListener('touchstart', openMobileMenu);
-    buttonClose.addEventListener('touchstart', closeMobileMenu);
-    document.addEventListener('touchstart', function (event) {
-      if (!topNavMenu.contains(event.target) && !buttonOpen.contains(event.target)) {
-        closeOnClick();
+    function closeOnClick(event) {
+      if (!topNavMenu.contains(event.target) && event.target !== buttonOpen) {
+        toggleMobileMenu();
       }
-    });
+    }
+  
+    buttonOpen.addEventListener('touchend', toggleMobileMenu);
+    buttonClose.addEventListener('touchend', toggleMobileMenu);
+    document.addEventListener('touchend', closeOnClick);
   })();
+  
