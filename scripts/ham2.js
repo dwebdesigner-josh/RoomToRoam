5da317d90545605ca2e30f288f65210342e67aa2
Josh Windows <greateagle87@gmail.com>
2024-06-10 06:34:44 -0400

    test 1millionandafuckingidk

--- scripts/ham2.js
+++ scripts/ham2.js
@@ -1,36 +1,59 @@
-(() => {
-    const buttonOpen = document.querySelector('#topnav-open');
-    const buttonClose = document.querySelector('#topnav-close');
-    const topNavMenu = document.querySelector('#topnav-menu');
+$(document).ready(function() {
+    const buttonOpen = $('#topnav-open');
+    const buttonClose = $('#topnav-close');
+    const topNavMenu = $('#topnav-menu');
+    const main = $('main');
+    const reviewLinks = $('.review-link');
   
     function openMobileMenu() {
-      topNavMenu.classList.add('open');
-      buttonOpen.setAttribute('aria-expanded', 'true');
+      console.log('Open menu function called');
+      buttonOpen.attr("aria-expanded", "true");
+      topNavMenu.removeAttr('inert').removeAttr('style');
+      buttonOpen.attr('inert', '');
       buttonClose.focus();
+  
+      reviewLinks.attr('tabindex', '-1');
     }
   
     function closeMobileMenu() {
-      topNavMenu.classList.remove('open');
-      buttonOpen.setAttribute('aria-expanded', 'false');
-      buttonOpen.focus();
+      console.log('Close menu function called');
+      buttonOpen.attr("aria-expanded", "false");
+      topNavMenu.attr('inert', '');
+      buttonOpen.removeAttr('inert').focus();
+  
+      reviewLinks.removeAttr('tabindex');
+  
+      setTimeout(function() {
+        topNavMenu.css('transition', 'none');
+      }, 500);
     }
   
-    function toggleMobileMenu() {
-      if (topNavMenu.classList.contains('open')) {
+    function closeOnClick() {
+      if (buttonOpen.attr('aria-expanded') === 'true') {
         closeMobileMenu();
+      }
+    }
+  
+    function setupTopNav(e) {
+      if (e.matches) {
+        // Mobile
+        topNavMenu.attr('inert', '').css('transition', 'none');
       } else {
-        openMobileMenu();
-      }
-    }
-  
-    function closeOnClick(event) {
-      if (!topNavMenu.contains(event.target) && event.target !== buttonOpen) {
+        // Tablet/Desktop
         closeMobileMenu();
+        topNavMenu.removeAttr('inert');
       }
     }
   
-    buttonOpen.addEventListener('touchend', toggleMobileMenu);
-    buttonClose.addEventListener('touchend', closeMobileMenu);
-    document.addEventListener('touchend', closeOnClick);
-  })();
+    const media = window.matchMedia('(max-width: 850px)');
+    setupTopNav(media);
+  
+    buttonOpen.on('click', openMobileMenu);
+    buttonClose.on('click', closeMobileMenu);
+    main.on('click', closeOnClick);
+  
+    media.addListener(function(e) {
+      setupTopNav(e);
+    });
+  });
   
