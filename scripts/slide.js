1bbccc3255d96a327e22697c14ab4b7cb0a4740c
Josh Windows <greateagle87@gmail.com>
2024-06-11 01:18:46 -0400

    test fix: safari doesn't seem to support the + CSS combinator for styles based on aria attributes, so removed that for slider and trying setattribute in slide.js to add the animation attribute upon button click - this may fix issue with hamburger as well

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,9 +1,10 @@
 (() => {
 
     const slideLeftButton = document.querySelector('#slide-left');
+    const slideImage1 = document.querySelector('#slide-1');
 
     function slideLeft(){
-        slideLeftButton.setAttribute("aria-pressed", "true");
+        slideImage1.setAttribute("animation", "bannermove 10s linear 1");
     }
 
     slideLeftButton.addEventListener('click', slideLeft);
