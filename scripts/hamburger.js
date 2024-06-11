313995cf341a6bb74cda2e96c87bd43bec70c4e2
Josh Windows <greateagle87@gmail.com>
2024-06-10 22:49:42 -0400

    test 2 failed - ended and will retry later - reverting back to previous commit ab31766ba04bf8e58478afbee01dc35a90e628c1  "test: github pages test"

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,92 +1,84 @@
 (() => {
 
-const buttonOpen = document.querySelector('#topnav-open');
-const buttonClose = document.querySelector('#topnav-close');
-//const media = window.matchMedia('(width < 850px)');
-const topNavMenu = document.querySelector('#topnav-menu');
-const main = document.querySelector('main');
-//const body = document.querySelector('body');
-//const review = document.querySelectorAll('.review-link'); //fixed: The querySelector() method only returns the first element that matches the specified selectors (first element in the class). To return all the matches, use the querySelectorAll() method instead.
-
-function openMobileMenu(){
-  alert('Open menu function called');
-  console.log('Open menu triggered');
-    buttonOpen.setAttribute("aria-expanded", "true");
- //   topNavMenu.removeAttribute('inert');
-   topNavMenu.removeAttribute('style');
- //   buttonOpen.setAttribute('inert','');
-                            // main.setAttribute('inert', '');       - TODO consider reimplementing or keeping removed
-
-//   buttonClose.focus();
-  // review.setAttribute('tabindex','-1');
-   
- //  review.forEach(nodelistitem => {
-   // nodelistitem.setAttribute('tabindex', '-1');
-  // });
-
-}
-
-function closeMobileMenu(){
-  alert('Close menu function called');
-    buttonOpen.setAttribute("aria-expanded", "false");
- //   topNavMenu.setAttribute('inert', '');
-                        //  main.removeAttribute('inert');        - TODO consider reimplementing or keeping removed
- //   buttonOpen.removeAttribute('inert');
-//    buttonOpen.focus();
-    //review.removeAttribute('tabindex');
-    
- 
- //   review.forEach(nodelistitem => {
-   //   nodelistitem.removeAttribute('tabindex');
- //   });
- 
-
-  setTimeout(() => {
-    topNavMenu.style.transition = 'none';
-  }, 500);
-}
-
-function closeonclick(){
-  if (buttonOpen.getAttribute('aria-expanded') === 'true'){
-    closeMobileMenu();
-  }
-  else {}
-};
-
-//if( $('.classname').getAttribute('aria-expanded') === 'true') {} https://stackoverflow.com/questions/37125674/how-to-check-for-attribute-value
-
-
-//function setupTopNav(e) {
-  //  if (e.matches) {
-              // is mobile
-    //  console.log('is mobile');
-      //topNavMenu.setAttribute('inert', '');
-      //topNavMenu.style.transition = 'none';
-    //} else {
-            // is tablet/desktop
-      //console.log('is desktop');
-      //closeMobileMenu();
-    //  topNavMenu.removeAttribute('inert');
-    //}
-  //}
+  const buttonopen = document.querySelector('#topnav-open');
+  const buttonclose = document.querySelector('#topnav-close');
+  const media = window.matchMedia('(width < 850px)');
+  const topNavMenu = document.querySelector('#topnav-menu');
+  const main = document.querySelector('main');
+  const body = document.querySelector('body');
+  const review = document.querySelectorAll('.review-link'); //fixed: The querySelector() method only returns the first element that matches the specified selectors (first element in the class). To return all the matches, use the querySelectorAll() method instead.
   
-  //setupTopNav(media);
-
-
-
-buttonOpen.addEventListener('click', openMobileMenu);
-buttonClose.addEventListener('click', closeMobileMenu);
-
-main.addEventListener('click', closeonclick);
-
-
-//media.addEventListener('change', function (e) {
-  //  setupTopNav(e);
-  //});
- 
-
-})();
-
-
-
-
+  function openMobileMenu(){
+      buttonopen.setAttribute("aria-expanded", "true");
+      topNavMenu.removeAttribute('inert');
+      topNavMenu.removeAttribute('style');
+      buttonopen.setAttribute('inert','');
+     // main.setAttribute('inert', '');       - TODO consider reimplementing or keeping removed
+     buttonclose.focus();
+    // review.setAttribute('tabindex','-1');
+     
+     review.forEach(nodelistitem => {
+      nodelistitem.setAttribute('tabindex', '-1');
+     });
+  
+  }
+  
+  function closeMobileMenu(){
+      buttonopen.setAttribute("aria-expanded", "false");
+      topNavMenu.setAttribute('inert', '');
+    //  main.removeAttribute('inert');        - TODO consider reimplementing or keeping removed
+      buttonopen.removeAttribute('inert');
+      buttonopen.focus();
+      //review.removeAttribute('tabindex');
+      
+   
+      review.forEach(nodelistitem => {
+        nodelistitem.removeAttribute('tabindex');
+      });
+   
+  
+    setTimeout(() => {
+      topNavMenu.style.transition = 'none';
+    }, 500);
+  }
+  
+  function closeonclick(){
+    if (buttonopen.getAttribute('aria-expanded') === 'true'){
+      closeMobileMenu();
+    }
+    else {}
+  };
+  
+  //if( $('.classname').getAttribute('aria-expanded') === 'true') {} https://stackoverflow.com/questions/37125674/how-to-check-for-attribute-value
+  
+  
+  function setupTopNav(e) {
+      if (e.matches) {
+        // is mobile
+        console.log('is mobile');
+        topNavMenu.setAttribute('inert', '');
+        topNavMenu.style.transition = 'none';
+      } else {
+        // is tablet/desktop
+        console.log('is desktop');
+        closeMobileMenu();
+        topNavMenu.removeAttribute('inert');
+      }
+    }
+    
+    setupTopNav(media);
+  
+  
+  
+  buttonopen.addEventListener('click', openMobileMenu);
+  buttonclose.addEventListener('click', closeMobileMenu);
+  
+  main.addEventListener('click', closeonclick);
+  
+  
+  media.addEventListener('change', function (e) {
+      setupTopNav(e);
+    });
+   
+  
+  })();
