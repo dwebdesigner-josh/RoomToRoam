ff7cb93b1251575524936d2d33c66072f5fe8444
Josh Windows <greateagle87@gmail.com>
2024-06-12 01:15:25 -0400

    feat: slide.js slideLeftCount1 function is working (cycling successfully from ..-1-2-3-1-.. for all 3 slides) - need to fix lack of movement from slides after first button pressing though

--- scripts/slide.js
+++ scripts/slide.js
@@ -3,6 +3,7 @@
     let slideLeftCount1 = 1
     let slideLeftCount2 = 2
     let slideLeftCount3 = 3
+    const totalSlides = 3
 
     const slideLeftButton = document.querySelector('#slide-left');
     const slideNumber = '#slide-';
@@ -15,7 +16,30 @@
         slideImage1.setAttribute("style", "transform: translateX(700px);");
         slideImage2.setAttribute("style", "transform: translateX(-350px);");
         slideImage3.setAttribute("style", "transform: translateX(-350px) scale(1.2); ");
-slideLeftCount1 +=1;
+if (slideLeftCount1 >= totalSlides) {
+        slideLeftCount1 = 1;
+        console.log('1 is back to 1');
+} else {
+    slideLeftCount1 += 1;
+    console.log('1 up');
+}
+
+if (slideLeftCount2 >= totalSlides) {
+    slideLeftCount2 = 1;
+    console.log('2 is back to 1');
+} else {
+slideLeftCount2 += 1;
+console.log('2 up');
+}
+
+if (slideLeftCount3 >= totalSlides) {
+    slideLeftCount3 = 1;
+    console.log('3 is back to 1');
+} else {
+slideLeftCount3 += 1;
+console.log('3 up');
+}
+
     }
 
 
