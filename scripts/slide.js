5c67eca31aff64a2822a2501a625206bbb45fc01
Josh Windows <greateagle87@gmail.com>
2024-06-12 02:18:12 -0400

    fix: slide.js cycle now applying to changes of slideImages with CSS translations/transformations in a cycle as required | TODO: fix cycle styles/issue with cycle positions not fitting in the carousel

--- scripts/slide.js
+++ scripts/slide.js
@@ -7,15 +7,10 @@
 
     const slideLeftButton = document.querySelector('#slide-left');
     const slideNumber = '#slide-';
-    var slideImage1 = document.querySelector(slideNumber+slideLeftCount1);
-    var slideImage2 = document.querySelector(slideNumber+slideLeftCount2);
-    var slideImage3 = document.querySelector(slideNumber+slideLeftCount3);
     
 
     function slideLeft(){
-        slideImage1.setAttribute("style", "transform: translateX(700px);");
-        slideImage2.setAttribute("style", "transform: translateX(-350px);");
-        slideImage3.setAttribute("style", "transform: translateX(-350px) scale(1.2); ");
+        
 if (slideLeftCount1 >= totalSlides) {
         slideLeftCount1 = 1;
         console.log('1 is back to 1');
@@ -40,6 +35,17 @@ slideLeftCount3 += 1;
 console.log('3 up');
 }
 
+
+var slideImage1 = document.querySelector(slideNumber+slideLeftCount1);
+var slideImage2 = document.querySelector(slideNumber+slideLeftCount2);
+var slideImage3 = document.querySelector(slideNumber+slideLeftCount3);
+
+slideImage1.removeAttribute("style");
+slideImage2.removeAttribute("style");
+slideImage3.removeAttribute("style");
+        slideImage1.setAttribute("style", "transform: translateX(700px);");
+        slideImage2.setAttribute("style", "transform: translateX(-350px);");
+        slideImage3.setAttribute("style", "transform: translateX(-350px) scale(1.2); ");
     }
 
 
