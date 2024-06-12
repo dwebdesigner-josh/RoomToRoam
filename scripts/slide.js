0c9e23edc3e260513586357ca699ff3a365949a7
Josh Windows <greateagle87@gmail.com>
2024-06-12 04:41:11 -0400

    feat: right slide button added to slide.js | fix: slide js right and left functions switched for UX improvement  | TODO container and item CSS for positioning and responsive design | TODO item additions to html and slide.js

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,54 +1,91 @@
 (() => {
 
-    let slideLeftCount1 = 1
-    let slideLeftCount2 = 2
-    let slideLeftCount3 = 3
+    let slideCount1 = 1
+    let slideCount2 = 2
+    let slideCount3 = 3
     const totalSlides = 3
 
     const slideLeftButton = document.querySelector('#slide-left');
+    const slideRightButton = document.querySelector('#slide-right');
     const slideNumber = '#slide-';
     
 
-    function slideLeft(){
+    function slideRight(){
         
-if (slideLeftCount1 >= totalSlides) {
-        slideLeftCount1 = 1;
-        console.log('1 is back to 1');
-} else {
-    slideLeftCount1 += 1;
-    console.log('1 up');
+                if (slideCount1 >= totalSlides) {
+                        slideCount1 = 1;
+                        console.log('1 is back to 1');
+                } else {
+                    slideCount1 += 1;
+                    console.log('1 up');
+                }
+
+                if (slideCount2 >= totalSlides) {
+                    slideCount2 = 1;
+                    console.log('2 is back to 1');
+                } else {
+                slideCount2 += 1;
+                console.log('2 up');
+                }
+
+                if (slideCount3 >= totalSlides) {
+                    slideCount3 = 1;
+                    console.log('3 is back to 1');
+                } else {
+                slideCount3 += 1;
+                console.log('3 up');
+                }
+
+
+    var slideImage1 = document.querySelector(slideNumber+slideCount1);
+    var slideImage2 = document.querySelector(slideNumber+slideCount2);
+    var slideImage3 = document.querySelector(slideNumber+slideCount3);
+
+        slideImage1.removeAttribute("style");
+        slideImage2.removeAttribute("style");
+        slideImage3.removeAttribute("style");
+                slideImage1.setAttribute("style", "order: 3; transform: translateX(-350px);");  //middle to left
+                slideImage2.setAttribute("style", "order: 4; transform: translateX(-350px) scale(1.2);"); // right to middle
+                slideImage3.setAttribute("style", "order: 2; transform: translateX(700px);"); //left to right
 }
 
-if (slideLeftCount2 >= totalSlides) {
-    slideLeftCount2 = 1;
-    console.log('2 is back to 1');
-} else {
-slideLeftCount2 += 1;
-console.log('2 up');
+function slideLeft(){
+                if (slideCount1 <= 1) {
+                    slideCount1 = 3;
+        //            console.log('1 is back to 1');
+                } else {
+                    slideCount1 -= 1;
+       //             console.log('1 up');
+                }
+
+                if (slideCount2 <= 1) {
+                    slideCount2 = 3;
+         //           console.log('2 is back to 1');
+                } else {
+                slideCount2 -= 1;
+           //     console.log('2 up');
+                }
+
+                if (slideCount3 <= 1) {
+                    slideCount3 = 3;
+         //           console.log('3 is back to 1');
+                } else {
+                slideCount3 -= 1;
+           //     console.log('3 up');
+                }
+    var slideImage1 = document.querySelector(slideNumber+slideCount1);
+    var slideImage2 = document.querySelector(slideNumber+slideCount2);
+    var slideImage3 = document.querySelector(slideNumber+slideCount3);
+
+        slideImage1.removeAttribute("style");
+        slideImage2.removeAttribute("style");
+        slideImage3.removeAttribute("style");
+                slideImage1.setAttribute("style", "order: 3; transform: translateX(-350px);"); 
+                slideImage2.setAttribute("style", "order: 4; transform: translateX(-350px) scale(1.2);"); 
+                slideImage3.setAttribute("style", "order: 2; transform: translateX(700px);"); 
 }
-
-if (slideLeftCount3 >= totalSlides) {
-    slideLeftCount3 = 1;
-    console.log('3 is back to 1');
-} else {
-slideLeftCount3 += 1;
-console.log('3 up');
-}
-
-
-var slideImage1 = document.querySelector(slideNumber+slideLeftCount1);
-var slideImage2 = document.querySelector(slideNumber+slideLeftCount2);
-var slideImage3 = document.querySelector(slideNumber+slideLeftCount3);
-
-slideImage1.removeAttribute("style");
-slideImage2.removeAttribute("style");
-slideImage3.removeAttribute("style");
-        slideImage1.setAttribute("style", "order: 3; transform: translateX(-350px);");  //middle to left
-        slideImage2.setAttribute("style", "order: 4; transform: translateX(-350px) scale(1.2);"); // right to middle
-        slideImage3.setAttribute("style", "order: 2; transform: translateX(700px);"); //left to right
-    }
 
 
     slideLeftButton.addEventListener('click', slideLeft);
-
+    slideRightButton.addEventListener('click', slideRight);
 })();
