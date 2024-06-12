30acaa949e748952833deb8de658aeec1d55cef6
Josh Windows <greateagle87@gmail.com>
2024-06-12 00:59:28 -0400

    feat: slide.js programming started for variable slide function on highlight reel

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,11 +1,24 @@
 (() => {
 
+    let slideLeftCount1 = 1
+    let slideLeftCount2 = 2
+    let slideLeftCount3 = 3
+
     const slideLeftButton = document.querySelector('#slide-left');
-    const slideImage1 = document.querySelector('#slide-1');
+    const slideNumber = '#slide-';
+    var slideImage1 = document.querySelector(slideNumber+slideLeftCount1);
+    var slideImage2 = document.querySelector(slideNumber+slideLeftCount2);
+    var slideImage3 = document.querySelector(slideNumber+slideLeftCount3);
+    
 
     function slideLeft(){
-        slideImage1.setAttribute("style", "animation: bannermove 10s linear 1;");
+        slideImage1.setAttribute("style", "transform: translateX(700px);");
+        slideImage2.setAttribute("style", "transform: translateX(-350px);");
+        slideImage3.setAttribute("style", "transform: translateX(-350px) scale(1.2); ");
+slideLeftCount1 +=1;
     }
 
+
     slideLeftButton.addEventListener('click', slideLeft);
+
 })();
