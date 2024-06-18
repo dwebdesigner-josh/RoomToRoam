b9909872ddc8ef14d9bc8d281762b4802d108ad5
Josh Windows <greateagle87@gmail.com>
2024-06-18 03:12:52 -0400

    test cont - simplified css and js for animation to suit my needs vs the source code

--- scripts/scrollanimation.js
+++ scripts/scrollanimation.js
@@ -4,8 +4,8 @@
   const scrolltest = document.querySelector('#scrolltest');
 
 window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+   /* document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));*/
     scrolltest.setAttribute('style','animation-play-state: running;');
-  }, false);
+  });
 
 })();
