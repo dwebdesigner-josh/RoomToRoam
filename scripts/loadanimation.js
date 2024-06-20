a6737cd2f9480de5a894298ed47da2d65355c7ee
Josh Windows <greateagle87@gmail.com>
2024-06-20 04:43:55 -0400

    feat: added js function for detecting first time loading of webpage to affect loadscreen animation time (quicker animation if not first visit)

---
+++ scripts/loadanimation.js
@@ -0,0 +1,19 @@
+(() => {
+// Check if it's the first time the user is visiting
+var firstTime = localStorage.getItem("first_time") === null;
+const loadScreen = document.querySelector('.loadscreen'); 
+
+function loadSpeedChanger {
+if(firstTime) {
+    // Perform actions for the first time load
+   alert("Welcome! This is your first visit.");    
+    // Set the flag in localStorage to indicate the user has visited
+    localStorage.setItem("first_time", "1");
+}
+
+else{
+    loadScreen.setAttribute("style","animation: scrolldown 5s linear 1;")
+}
+}
+
+})();
