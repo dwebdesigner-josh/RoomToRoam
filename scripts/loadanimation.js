6f93a78c8fa8702c48b16de51ccb4353d130475d
Josh Windows <greateagle87@gmail.com>
2024-06-20 06:39:55 -0400

    tests completed successfully: loadscreen now working completely on all devices without issues | TODO: fine-tune animation time for first load (may need to be slightly longer) | feat: cleaned up CSS and JS now that a working version has been produced and alternate versions are obsolete

--- scripts/loadanimation.js
+++ scripts/loadanimation.js
@@ -12,9 +12,6 @@ if(firstTime) {
 
 else{
     loadScreen.setAttribute("id","loadscreen-notfirst");
-   // loadScreen.style.animation = "scrolldown 5s linear 1"; - issue with all animation related styles being overwritten as well, so would need to add a bunch of extra code anyway, so maybe just doing the below instead  
-   // loadScreen.setAttribute("style","animation: scrolldown 5s linear 1;") - issue with this is you'd have to readd all styles plus the changed animations style or else all others would be overwritten
-   // loadScreen.setAttribute("style","width: 100%;height: 100%;background-image: url(/RoomToRoam/images/starV1frame1.png);background-repeat: repeat;  position: absolute;top: 0;transform: translateY(0);z-index: 20;display: flex;justify-content: center;animation: scrolldown 10s linear 1;animation-play-state: running;animation-iteration-count: 1;animation-fill-mode: both;")
 }
 }
 
