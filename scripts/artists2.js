10e0ea6e212755fe573f57c3e3d01aca53082e4b
Josh Windows <greateagle87@gmail.com>
2024-07-22 06:24:35 -0400

    test: github pages

--- scripts/artists2.js
+++ scripts/artists2.js
@@ -69,11 +69,11 @@ function expand() {
     if (!expanded) {
         expanded = true;
         artistDetails.setAttribute("aria-expanded", "true");
-        expandArrow.setAttribute("src", "/images/expandarrowup.png");
+        expandArrow.setAttribute("src", "/RoomToRoam/images/expandarrowup.png");
     } else {
         expanded = false;
         artistDetails.setAttribute("aria-expanded", "false");
-        expandArrow.setAttribute("src", "/images/expandarrowdown.png");
+        expandArrow.setAttribute("src", "/RoomToRoam/images/expandarrowdown.png");
     }
 }
 
