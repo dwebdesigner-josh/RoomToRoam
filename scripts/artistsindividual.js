8e3db7efb9efe860f949a9ec87cb6a7223cf493b
Josh Windows <greateagle87@gmail.com>
2024-08-13 04:07:23 -0400

    feat: albums slide info expansion functions created

--- scripts/artistsindividual.js
+++ scripts/artistsindividual.js
@@ -103,12 +103,12 @@ mediaChange(mediaQuery);
                 slideImage5.setAttribute("style", " z-index: 28; left: 60%; "); 
                     slideImage1.setAttribute("aria-expanded", "false");
                     slideImage1.setAttribute("inert", "");
-                    slideImage2.setAttribute("aria-expanded", "false");
-                    slideImage2.setAttribute("inert", "");
+                    slideImage2.setAttribute("aria-expanded", "true");
+                    slideImage2.removeAttribute("inert", "");
                     slideImage3.setAttribute("aria-expanded", "true");
                     slideImage3.removeAttribute("inert", "");
-                    slideImage4.setAttribute("aria-expanded", "false");
-                    slideImage4.setAttribute("inert", "");
+                    slideImage4.setAttribute("aria-expanded", "true");
+                    slideImage4.removeAttribute("inert", "");
                     slideImage5.setAttribute("aria-expanded", "false");
                     slideImage5.setAttribute("inert", "");
             }
@@ -159,12 +159,12 @@ function slideLeft(){
             slideImage5.setAttribute("style", " z-index: 29; left: 60%; "); 
                 slideImage1.setAttribute("aria-expanded", "false");
                 slideImage1.setAttribute("inert","");
-                slideImage2.setAttribute("aria-expanded", "false");
-                slideImage2.setAttribute("inert","");   
+                slideImage2.setAttribute("aria-expanded", "true");
+                slideImage2.removeAttribute("inert","");   
                 slideImage3.setAttribute("aria-expanded", "true");
                 slideImage3.removeAttribute("inert","");
-                slideImage4.setAttribute("aria-expanded", "false");
-                slideImage4.setAttribute("inert", "");
+                slideImage4.setAttribute("aria-expanded", "true");
+                slideImage4.removeAttribute("inert", "");
                 slideImage5.setAttribute("aria-expanded", "false");
                 slideImage5.setAttribute("inert", "");             
 
@@ -200,18 +200,173 @@ function slideLeftSingle(){
     }, transitionTime);
     stopSlide();
 }
-    slideLeftButton.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter' || event.keyCode === 13) {
-            slideLeftSingle();
-        }
-    });
-    slideRightButton.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter' || event.keyCode === 13) {
-            slideRightSingle();
-        }
-    });
+
+
+
+
+                slideLeftButton.addEventListener('keydown', function(event) {
+                    if (event.key === 'Enter' || event.keyCode === 13) {
+                        slideLeftSingle();
+                    }
+                });
+                slideRightButton.addEventListener('keydown', function(event) {
+                    if (event.key === 'Enter' || event.keyCode === 13) {
+                        slideRightSingle();
+                    }
+                });
                 slideLeftButton.addEventListener('click', slideLeftSingle);
                 slideRightButton.addEventListener('click', slideRightSingle);
+                
+
 })();
 
+(() => {
+    const slideExpand1 = document.querySelector('#slide-21');
+    const slideExpand2 = document.querySelector('#slide-22');
+    const slideExpand3 = document.querySelector('#slide-23');
+    const slideExpand4 = document.querySelector('#slide-24');
+    const slideExpand5 = document.querySelector('#slide-25');
+    const slideInfo1 = document.querySelector('#slideinfo-1');
+    const slideInfo2 = document.querySelector('#slideinfo-2');
+    const slideInfo3 = document.querySelector('#slideinfo-3');
+    const slideInfo4 = document.querySelector('#slideinfo-4');
+    const slideInfo5 = document.querySelector('#slideinfo-5');
+ //   const slideLeftButton2 = document.querySelector('#slide-left2');
+   // const slideRightButton2 = document.querySelector('#slide-right2');
+let expanded1 = false;
+let expanded2 = false;
+let expanded3 = false;
+let expanded4 = false;
+let expanded5 = false;
 
+slideExpand1.addEventListener('click',
+    function(){
+if(!expanded1){
+    expanded1=true;
+    slideInfo1.setAttribute('style','display: block; visibility: visible;');
+    slideInfo1.setAttribute('aria-expanded','true');
+} else {
+    expanded1 = false;
+    slideInfo1.setAttribute('style','display: none; visibility: hidden;');
+    slideInfo1.setAttribute('aria-expanded','false');
+}
+expanded2 = false;
+slideInfo2.setAttribute('style','display: none; visibility: hidden;');
+slideInfo2.setAttribute('aria-expanded','false');
+expanded3 = false;
+slideInfo3.setAttribute('style','display: none; visibility: hidden;');
+slideInfo3.setAttribute('aria-expanded','false');
+expanded4 = false;
+slideInfo4.setAttribute('style','display: none; visibility: hidden;');
+slideInfo4.setAttribute('aria-expanded','false');
+expanded5 = false;
+slideInfo5.setAttribute('style','display: none; visibility: hidden;');
+slideInfo5.setAttribute('aria-expanded','false');
+    }
+);
+
+slideExpand2.addEventListener('click',
+    function(){
+if(!expanded2){
+    expanded2=true;
+    slideInfo2.setAttribute('style','display: block; visibility: visible;');
+    slideInfo2.setAttribute('aria-expanded','true');
+} else {
+    expanded2 = false;
+    slideInfo2.setAttribute('style','display: none; visibility: hidden;');
+    slideInfo2.setAttribute('aria-expanded','false');
+}
+expanded1 = false;
+slideInfo1.setAttribute('style','display: none; visibility: hidden;');
+slideInfo1.setAttribute('aria-expanded','false');
+expanded3 = false;
+slideInfo3.setAttribute('style','display: none; visibility: hidden;');
+slideInfo3.setAttribute('aria-expanded','false');
+expanded4 = false;
+slideInfo4.setAttribute('style','display: none; visibility: hidden;');
+slideInfo4.setAttribute('aria-expanded','false');
+expanded5 = false;
+slideInfo5.setAttribute('style','display: none; visibility: hidden;');
+slideInfo5.setAttribute('aria-expanded','false');
+    }
+);
+
+slideExpand3.addEventListener('click',
+    function(){
+if(!expanded3){
+    expanded3=true;
+    slideInfo3.setAttribute('style','display: block; visibility: visible;');
+    slideInfo3.setAttribute('aria-expanded','true');
+} else {
+    expanded3 = false;
+    slideInfo3.setAttribute('style','display: none; visibility: hidden;');
+    slideInfo3.setAttribute('aria-expanded','false');
+}
+expanded2 = false;
+slideInfo2.setAttribute('style','display: none; visibility: hidden;');
+slideInfo2.setAttribute('aria-expanded','false');
+expanded1 = false;
+slideInfo1.setAttribute('style','display: none; visibility: hidden;');
+slideInfo1.setAttribute('aria-expanded','false');
+expanded4 = false;
+slideInfo4.setAttribute('style','display: none; visibility: hidden;');
+slideInfo4.setAttribute('aria-expanded','false');
+expanded5 = false;
+slideInfo5.setAttribute('style','display: none; visibility: hidden;');
+slideInfo5.setAttribute('aria-expanded','false');
+    }
+);
+
+slideExpand4.addEventListener('click',
+    function(){
+if(!expanded4){
+    expanded4=true;
+    slideInfo4.setAttribute('style','display: block; visibility: visible;');
+    slideInfo4.setAttribute('aria-expanded','true');
+} else {
+    expanded4 = false;
+    slideInfo4.setAttribute('style','display: none; visibility: hidden;');
+    slideInfo4.setAttribute('aria-expanded','false');
+}
+expanded2 = false;
+slideInfo2.setAttribute('style','display: none; visibility: hidden;');
+slideInfo2.setAttribute('aria-expanded','false');
+expanded3 = false;
+slideInfo3.setAttribute('style','display: none; visibility: hidden;');
+slideInfo3.setAttribute('aria-expanded','false');
+expanded1 = false;
+slideInfo1.setAttribute('style','display: none; visibility: hidden;');
+slideInfo1.setAttribute('aria-expanded','false');
+expanded5 = false;
+slideInfo5.setAttribute('style','display: none; visibility: hidden;');
+slideInfo5.setAttribute('aria-expanded','false');
+    }
+);
+
+slideExpand5.addEventListener('click',
+    function(){
+if(!expanded5){
+    expanded5=true;
+    slideInfo5.setAttribute('style','display: block; visibility: visible;');
+    slideInfo5.setAttribute('aria-expanded','true');
+} else {
+    expanded5 = false;
+    slideInfo5.setAttribute('style','display: none; visibility: hidden;');
+    slideInfo5.setAttribute('aria-expanded','false');
+}
+expanded2 = false;
+slideInfo2.setAttribute('style','display: none; visibility: hidden;');
+slideInfo2.setAttribute('aria-expanded','false');
+expanded3 = false;
+slideInfo3.setAttribute('style','display: none; visibility: hidden;');
+slideInfo3.setAttribute('aria-expanded','false');
+expanded4 = false;
+slideInfo4.setAttribute('style','display: none; visibility: hidden;');
+slideInfo4.setAttribute('aria-expanded','false');
+expanded1 = false;
+slideInfo1.setAttribute('style','display: none; visibility: hidden;');
+slideInfo1.setAttribute('aria-expanded','false');
+    }
+);
+
+})();
