d4e017968392a8a60d2165a885edf1aead4bbaf4
Josh Windows <greateagle87@gmail.com>
2024-09-10 06:35:35 -0400

    test cont

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT; // Use environment variable
   const formSubmitLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 3, // Limit each IP to 3 requests per `windowMs`
-    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    standardHeaders: true, // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     message: 'Too many form submissions from this device, please try again later.',
   });
@@ -26,8 +26,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-// Route to handle form submission
-app.post('/send-email', 
+// Route to handle form submission, run express rate limiter 
+app.post('/send-email', formSubmitLimiter,
 
      // Honeypot check
      (req, res, next) => {
@@ -37,16 +37,15 @@ app.post('/send-email',
         next();
     },
 
-    // Apply daily rate limiter to the route
-    formSubmitLimiter,
+    
 
     // validation middleware
     [
       body('subject').isLength({ min: 1 }).withMessage('Subject is required'),
       body('body').isLength({ min: 1 }).withMessage('Message body is required')
     ],
-    async (req, res) => {
 
+    async (req, res) => {
       const errors = validationResult(req); //Check validation errors
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
