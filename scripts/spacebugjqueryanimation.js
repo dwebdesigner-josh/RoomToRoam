9a2b839d683ecf3499a9c8ce573515dcf07d9aba
Josh Windows <greateagle87@gmail.com>
2024-09-28 04:16:35 -0400

    feat: about page spacebug animation created and implemented with jquery animation script

---
+++ scripts/spacebugjqueryanimation.js
@@ -0,0 +1,61 @@
+// inspiration: https://www.sitepoint.com/frame-by-frame-animation-css-javascript/#1framebyframeanimationbychangingtheimagessource
+(() => {
+    //CHANGE class of $element below to match the HTML class of the img we're targeting
+    const $element = $('.spacebug-img');
+    //CHANGE the path below to match the folder containing the src imgs
+    const imagePath = '/scripts/swirl1/spacebug';
+    //TEST PATH TO MATCH GITHUB PAGES: '/scripts....' instead of '/scripts....'
+    const totalFrames = 19;
+    const animationDuration = 1373;
+    // TODO - figure out a proper formula for converting FPS into the above numbers (ratio is 99 / 7150)
+    const timePerFrame = animationDuration / totalFrames;
+    let timeWhenLastUpdate;
+    let timeFromLastUpdate;
+    //CHANGE number set here is the starting frame number
+    let frameNumber = 1;
+    
+    // 'step' function will be called each time browser rerender the content
+    // we achieve that by passing 'step' as a parameter to the 'requestAnimationFrame' function
+    function step(startTime) {
+      // 'startTime' is provided by requestAnimationName function, and we can consider it as current time
+      // first of all we calculate how much time has passed from the last time when frame was update
+      if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+      timeFromLastUpdate = startTime - timeWhenLastUpdate;
+      
+      // then we check if it is time to update the frame
+      if (timeFromLastUpdate > timePerFrame) {
+        // and update it accordingly
+        //CHANGE name and extension in +'' field accordingly to match sequence naming system
+        $element.attr('src', imagePath + `/spacebugseq${frameNumber}.jpg`);
+        // reset the last update time
+        timeWhenLastUpdate = startTime;
+        
+        // then increase the frame number or reset it if it is the last frame
+        if (frameNumber >= totalFrames) {
+          frameNumber = 1;
+        } else {
+          frameNumber = frameNumber + 1;
+        }        
+      }
+      
+      requestAnimationFrame(step);
+    }
+    
+    // create a set of hidden divs - and set their background-image attribute to required images - that will force browser to download the images
+    //CHANGE path/image name to match, IF MULTIPLE JQUERY ANIMATIONS CHANGE DIV ID FORMAT TO BE UNIQUE TO EACH - unless using the same img base for each animation, in which case REMOVE this from all but one script
+    $(document).ready(() => {
+      for (var i = 1; i < totalFrames + 1; i++) {
+        $('body').append(`<div id="preload-image-${i}" style="background-image: url('${imagePath}/spacebugseq${i}.jpg');"></div>`);
+      }
+    });
+    
+    // start animation after waiting for page content to load
+    $(window).on('load', () => {
+      requestAnimationFrame(step);
+    });
+    
+    // TODO - consider adding a delay timer to the above ^ to avoid visible buffering 
+    
+    
+    })();
+    
